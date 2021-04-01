c2ac6e1e6ed80e2e790c1a51dfb5895b97bd8b25
Mostafa Tamim Mahmud <mostafatamim.mahmud7@gmail.com>
2021-04-02 01:21:55 +1100

    userFlow diagram and wireframe section

--- assets/js/projects_js.js
+++ assets/js/projects_js.js
@@ -19,3 +19,32 @@ function checkScrollDirectionIsUp(event) {
 let scrollableElement = document.body;
 
 scrollableElement.addEventListener("wheel", checkScrollDirection);
+
+const burger = document.querySelector(".burger");
+function burgerToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 6, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: 0, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".burger-nav-bar", 0.5, {
+      clipPath: "circle(2500px at 100% -10%)",
+    });
+    gsap.to(".internal_navigation", { transform: "translateY(100px)" });
+    document.querySelector(".content_social_fixed").classList.toggle("show");
+    document.querySelector(".content_social_fixed").classList.toggle("hide");
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "black" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".burger-nav-bar", 0.5, { clipPath: "circle(50px at 100% -10%)" });
+    gsap.to(".internal_navigation", { transform: "translateY(0px)" });
+    document.querySelector(".content_social_fixed").classList.toggle("show");
+    document.querySelector(".content_social_fixed").classList.toggle("hide");
+    document.body.classList.remove("hide");
+  }
+}
+
+burger.addEventListener("click", burgerToggle);
